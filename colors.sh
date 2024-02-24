0d93aeacf8123da5542a926fd379d72340e5aa33
tenrimsos <31085027+tenrimsos@users.noreply.github.com>
2024-02-24 00:19:25 -0600

    Colors

---
+++ colors.sh
@@ -0,0 +1,13 @@
+echo "blanco"; tput sgr0 ; tput setaf 7
+sleep 3
+echo "azul"; tput sgr0 ; tput setaf 4
+sleep 3
+echo "verde"; tput sgr0 ; tput setaf 2
+sleep 3
+echo "magenta"; tput sgr0 ; tput setaf 5
+sleep 3
+echo "blanco"; tput sgr0 ; tput setaf 7
+sleep 3
+echo "amarillo"; tput sgr0 ; tput setaf 3
+sleep 3
+echo "rojo"; tput sgr0 ; tput setaf 1
